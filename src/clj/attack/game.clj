db75c5e4e95610a47e69c365a5697195cda9cc9a
Jamie Ly <me@jamie.ly>
2013-02-06 21:50:30 -0500

    Refactored grid to contain additional information

--- src/clj/attack/game.clj
+++ src/clj/attack/game.clj
@@ -14,31 +14,33 @@
 (defn complex-block[blocks]
   {:blocks blocks})
 
-(defn grid [blocks last-row]
-  {:blocks blocks :last-row last-row})
-
 (defn block-types []
   [:orange :yellow :pink :cyan :green :purple :red])
 
-(defn x-range []
-  (range 1 7))
-
-(defn grid-add-block-row [{blocks :blocks last-row :last-row}]
+(defn grid-add-block-row [{blocks :blocks
+                           rows :rows
+                           cols :cols}]
   (let [types (block-types)
-        new-last (+ last-row 1)
+        new-last (+ rows 1)
         new-block (fn [x]
                     (simple-block (point x new-last) (rand-nth types)))
-        new-blocks (map new-block (x-range))]
-    (grid (concat blocks new-blocks) new-last)))
+        new-blocks (map new-block (range 1 (+ cols 1)))]
+    {:blocks (concat blocks new-blocks)
+     :rows new-last
+     :cols cols}))
 
-(defn default-grid []
+(defn default-grid [cols rows]
   (let [types (block-types)
-        xs (x-range)]
-    (grid (for [x xs
-                y (range 1 7)]
-            (simple-block (point x y) (rand-nth types))) (reduce max xs))))
+        xs (range 1 (+ cols 1))]
+    {:blocks (for [x xs
+                y (range 1 (+ rows 1))]
+               (simple-block (point x y) (rand-nth types)))
+     :cols cols
+     :rows rows}))
   
 (defn default-game []
-  {:grid (default-grid)})
+  (let [rows 6
+        cols 6]
+    {:grid (default-grid cols rows)}))
 
    
