1f0fc2555cff6cf31603a09790f152fa891b56a2
Jamie Ly <me@jamie.ly>
2013-02-23 09:57:39 -0500

    Moved grid tests to appropriate namespace

--- test/attack/test/core.clj
+++ test/attack/test/core.clj
@@ -19,28 +19,6 @@
 (deftest complex-block
   (is (= (blk/new-complex [1 2 3])
          {:blocks [1 2 3]})))
-
-(deftest grid-empty
-  (is (= (grid/empty-grid 6)
-         {:blocks []
-          :rows 0
-          :cols 6})))
-
-(deftest grid-add-row
-  "Tests adding a new row to a grid"
-  (let [cols 6
-        {blocks :blocks :as new-grid-orig} (grid/add-row (grid/empty-grid cols))
-        ;; Use to change the type of the block (required since when we
-        ;; add a row, the block values are random.
-        swapped-blocks (map #(assoc % :type :red) blocks)
-        new-grid (assoc new-grid-orig :blocks swapped-blocks)
-        test-blocks (map #(blk/new-simple (pt/point % 1) :red)
-                         (range 1 (+ cols 1)))]
-    (is (= new-grid
-           {:blocks test-blocks
-            :rows 1
-            :cols cols}))))
-
 (deftest mod-clock
   "Test an internal function that is used to test clock ticks"
   (is (game/mod-clock? {:clock 10} 10))
@@ -108,86 +86,8 @@
     (is (= (blk/resolve-swaps nodissolve-blocks)
            nodissolve-blocks))))
 
-(deftest grid-swap-blocks
-  "Tests creating swap blocks out of two other blocks"
-  (let [blk #(blk/new-simple (pt/point %1 %2) :notype)
-        a (blk 1 2)
-        b (blk 3 4)
-        c (blk 5 6)
-        grid {:blocks [a b c]}]
-    (is (= (get (grid/swap-blocks grid a b) :blocks)
-           [c {:blocks [a b] :ticks (blk/swap-block-default-ticks) :type :swap}]))))
-
-(deftest grid-block-at
-  (let [blk #(blk/new-simple (pt/point %1 %2) :notype)
-        [a b c] [(blk 1 2) (blk 2 3) (blk 4 5)]
-        grid {:blocks [a b c]}
-        blk-at #(grid/block-at grid (pt/point %1 %2))
-        ]
-    (is (= (blk-at 2 3) b))
-    (is (= (blk-at 4 5) c))
-    (is (= (blk-at 9 9) nil))))
 
 (deftest gi-step
   (let [g (gi/default)]
     (is (not (nil? (gi/step g))))))
-
-(deftest grid-match-blocks-horizontal
-  "Tests removing matches from a grid"
-  (let [b #(blk/new-simple (pt/point %1 %2) %3)
-        blocks [(b 1 1 :red) (b 2 1 :red) (b 3 1 :red) (b 4 1 :blue)]
-        grid {:blocks blocks}
-        matches #{#{(b 1 1 :red) (b 2 1 :red) (b 3 1 :red)}}]
-    (is (= (grid/match-sets grid)
-           matches))))
-
-(deftest grid-match-blocks-vertical
-  "Tests removing matches from a grid"
-  (let [b #(blk/new-simple (pt/point %1 %2) %3)
-        blocks [(b 1 1 :red) (b 1 2 :red) (b 1 3 :red) (b 1 4 :blue)]
-        grid {:blocks blocks}
-        matches #{#{(b 1 1 :red) (b 1 2 :red) (b 1 3 :red)}}]
-    (is (= (grid/match-sets grid)
-           matches))))
-
-
-(deftest grid-no-match-blocks-vertical
-  "Tests removing matches from a grid"
-  (let [b #(blk/new-simple (pt/point %1 %2) %3)
-        blocks [(b 1 1 :red) (b 1 2 :blue) (b 1 3 :red) (b 1 4 :blue)]
-        grid {:blocks blocks}
-        matches #{}]
-    (is (= (grid/match-sets grid)
-           matches))))
-
-(deftest grid-match-blocks-vertical-and-horizontal
-  "Tests removing matches from a grid"
-  (let [b #(blk/new-simple (pt/point %1 %2) %3)
-        blocks [(b 1 1 :red)
-                (b 1 2 :red) (b 1 3 :red) (b 1 4 :blue)
-                (b 2 1 :red) (b 3 1 :red) (b 4 1 :blue)]
-        grid {:blocks blocks}
-        matches #{#{(b 1 1 :red) (b 1 2 :red) (b 1 3 :red) (b 2 1 :red) (b 3 1 :red)}}]
-    (is (= (grid/match-sets grid)
-           matches))))
-
-(deftest grid-condense-matchset
-  (let [orig-match-set #{#{{:type :red, :position [3 1]}
-                           {:type :red, :position [1 3]}
-                           {:type :red, :position [1 2]}
-                           {:type :red, :position [1 1]}
-                           {:type :red, :position [2 1]}}
-                         #{{:type :red, :position [3 1]}
-                           {:type :red, :position [1 1]}
-                           {:type :red, :position [2 1]}}
-                         #{{:type :red, :position [1 3]}
-                           {:type :red, :position [1 2]}
-                           {:type :red, :position [1 1]}}}
-        expected-match-set #{#{{:type :red, :position [3 1]}
-                               {:type :red, :position [1 3]}
-                               {:type :red, :position [1 2]}
-                               {:type :red, :position [1 1]}
-                               {:type :red, :position [2 1]}}}]
-    (is (= (grid/condense-match-set2 orig-match-set)
-           expected-match-set))))
 
--- test/attack/test/grid.clj
+++ test/attack/test/grid.clj
@@ -83,3 +83,106 @@
         new-grid (grid/add-blocks grid #{block})]
     (is (= grid
            (grid/remove-blocks new-grid #{block})))))
+
+(deftest swap-blocks
+  "Tests creating swap blocks out of two other blocks"
+  (let [blk #(blk/new-simple (pt/point %1 %2) :notype)
+        a (blk 1 2)
+        b (blk 3 4)
+        c (blk 5 6)
+        grid {:blocks [a b c]}]
+    (is (= (get (grid/swap-blocks grid a b) :blocks)
+           [c {:blocks [a b] :ticks (blk/swap-block-default-ticks) :type :swap}]))))
+
+(deftest block-at
+  (let [blk #(blk/new-simple (pt/point %1 %2) :notype)
+        [a b c] [(blk 1 2) (blk 2 3) (blk 4 5)]
+        grid {:blocks [a b c]}
+        blk-at #(grid/block-at grid (pt/point %1 %2))
+        ]
+    (is (= (blk-at 2 3) b))
+    (is (= (blk-at 4 5) c))
+    (is (= (blk-at 9 9) nil))))
+
+(deftest match-blocks-horizontal
+  "Tests removing matches from a grid"
+  (let [b #(blk/new-simple (pt/point %1 %2) %3)
+        blocks [(b 1 1 :red) (b 2 1 :red) (b 3 1 :red) (b 4 1 :blue)]
+        grid {:blocks blocks}
+        matches #{#{(b 1 1 :red) (b 2 1 :red) (b 3 1 :red)}}]
+    (is (= (grid/match-sets grid)
+           matches))))
+
+(deftest match-blocks-vertical
+  "Tests removing matches from a grid"
+  (let [b #(blk/new-simple (pt/point %1 %2) %3)
+        blocks [(b 1 1 :red) (b 1 2 :red) (b 1 3 :red) (b 1 4 :blue)]
+        grid {:blocks blocks}
+        matches #{#{(b 1 1 :red) (b 1 2 :red) (b 1 3 :red)}}]
+    (is (= (grid/match-sets grid)
+           matches))))
+
+
+(deftest no-match-blocks-vertical
+  "Tests removing matches from a grid"
+  (let [b #(blk/new-simple (pt/point %1 %2) %3)
+        blocks [(b 1 1 :red) (b 1 2 :blue) (b 1 3 :red) (b 1 4 :blue)]
+        grid {:blocks blocks}
+        matches #{}]
+    (is (= (grid/match-sets grid)
+           matches))))
+
+(deftest match-blocks-vertical-and-horizontal
+  "Tests removing matches from a grid"
+  (let [b #(blk/new-simple (pt/point %1 %2) %3)
+        blocks [(b 1 1 :red)
+                (b 1 2 :red) (b 1 3 :red) (b 1 4 :blue)
+                (b 2 1 :red) (b 3 1 :red) (b 4 1 :blue)]
+        grid {:blocks blocks}
+        matches #{#{(b 1 1 :red) (b 1 2 :red) (b 1 3 :red) (b 2 1 :red) (b 3 1 :red)}}]
+    (is (= (grid/match-sets grid)
+           matches))))
+
+(deftest condense-matchset
+  (let [orig-match-set #{#{{:type :red, :position [3 1]}
+                           {:type :red, :position [1 3]}
+                           {:type :red, :position [1 2]}
+                           {:type :red, :position [1 1]}
+                           {:type :red, :position [2 1]}}
+                         #{{:type :red, :position [3 1]}
+                           {:type :red, :position [1 1]}
+                           {:type :red, :position [2 1]}}
+                         #{{:type :red, :position [1 3]}
+                           {:type :red, :position [1 2]}
+                           {:type :red, :position [1 1]}}}
+        expected-match-set #{#{{:type :red, :position [3 1]}
+                               {:type :red, :position [1 3]}
+                               {:type :red, :position [1 2]}
+                               {:type :red, :position [1 1]}
+                               {:type :red, :position [2 1]}}}]
+    (is (= (grid/condense-match-set2 orig-match-set)
+           expected-match-set))))
+
+
+
+(deftest empty
+  (is (= (grid/empty-grid 6)
+         {:blocks []
+          :rows 0
+          :cols 6})))
+
+(deftest add-row
+  "Tests adding a new row to a grid"
+  (let [cols 6
+        {blocks :blocks :as new-grid-orig} (grid/add-row (grid/empty-grid cols))
+        ;; Use to change the type of the block (required since when we
+        ;; add a row, the block values are random.
+        swapped-blocks (map #(assoc % :type :red) blocks)
+        new-grid (assoc new-grid-orig :blocks swapped-blocks)
+        test-blocks (map #(blk/new-simple (pt/point % 1) :red)
+                         (range 1 (+ cols 1)))]
+    (is (= new-grid
+           {:blocks test-blocks
+            :rows 1
+            :cols cols}))))
+
